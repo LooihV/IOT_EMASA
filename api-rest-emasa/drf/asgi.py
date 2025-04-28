0acc74a2638693507178cbb2e4112dc424498a74
Carlos Mario Bernal Cuellar <124719570+carlosMario18@users.noreply.github.com>
2025-04-28 16:23:01 -0500

    Add files via upload

---
+++ api-rest-emasa/drf/asgi.py
@@ -0,0 +1,27 @@
+"""
+ASGI config for drf project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+import api.routing
+from django.urls import path
+from api.consumers import OCPPConsumer
+
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'drf.settings')
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": URLRouter(
+        [path("ws/datos/",OCPPConsumer.as_asgi()),
+         ]
+    ),
+})#MQTT
