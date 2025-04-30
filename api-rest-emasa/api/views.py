e129b35477ab7216365669610172bbaf500e04a4
Carlos <2420201003@estudiantesunibague.edu.co>
2025-04-30 13:04:47 -0500

    update user authentication

--- api-rest-emasa/api/views.py
+++ api-rest-emasa/api/views.py
@@ -132,8 +132,13 @@ User = get_user_model()
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [IsAdminUser]
+    permission_classes = [IsAuthenticated]
     
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_superuser:
+            return User.objects.all()
+        return User.objects.filter(id=user.id)
     
     
     
